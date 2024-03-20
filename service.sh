2dd4af67ff44554513e267f8fd8d78b40b8498d3
Coworking Metz <contact@coworking-metz.fr>
2024-03-20 22:16:52 +0100

    pas de log

--- service.sh
+++ service.sh
@@ -3,7 +3,7 @@ BASEDIR=$(dirname "$0")
 
 # Infinite loop
 while true; do
-    "$BASEDIR/traitement-webhooks.sh" >> "$BASEDIR/log/traitement-webhooks.log" 2>&1
-#   "$BASEDIR/traitement-webhooks.sh"
+#    "$BASEDIR/traitement-webhooks.sh" >> "$BASEDIR/log/traitement-webhooks.log" 2>&1
+   "$BASEDIR/traitement-webhooks.sh"
   sleep 5
 done
